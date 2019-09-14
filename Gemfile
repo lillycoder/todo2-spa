9248a0db4971bf3d560ce219fca0fc2f3fc18a9a
lillycoder <ltheinh@gmail.com>
2019-09-14 17:02:15 +0000

    finished lesson 12

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'bootsnap', '>= 1.1.0', require: false
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
+  gem 'rspec-rails', '~> 3.5'
+  gem 'factory_bot_rails'
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
 
@@ -46,8 +48,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
-  gem 'rspec-rails', '~> 3.5'
-  gem 'factory_bot_rails'
+  
 end
 
 group :test do
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,16 +1,19 @@
 require 'rails_helper'
-require 'factory_bot'
-
 
 RSpec.describe TasksController, type: :controller do
   describe "tasks#index" do
     it "should list the tasks in the database" do
       task1 = FactoryBot.create( :task)
       task2 = FactoryBot.create( :task)
+      task1.update_attributes(title: "Something else")
       get :index
       expect(response).to have_http_status :success
       response_value = ActiveSupport::JSON.decode(@response.body)
       expect(response_value.count).to eq(2)
+      response_ids = response_value.collect do |task|
+        task["id"]
+      end
+      expect(response_ids).to eq([task1.id, task2.id])
     end
   end
 
@@ -18,7 +21,7 @@ RSpec.describe TasksController, type: :controller do
     it "should allow tasks to be marked as done" do
       task = FactoryBot.create(:task, done: false)
       put :update, params: {id: task.id, task: { done: true}}
-      expect(respoonse).to have_http_status(:success)
+      expect(response).to have_http_status(:success)
       task.reload
       expect(task.done).to eq(true)
     end
